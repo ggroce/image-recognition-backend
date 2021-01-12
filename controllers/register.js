e696750f83d9f0162b04e3e81f6c240228b634ce
ggroce <ggrocedev@gmail.com>
2021-01-11 18:54:57 -0600

    debug

--- controllers/register.js
+++ controllers/register.js
@@ -7,6 +7,8 @@ const handleRegister = (req, res, db, bcrypt) => {
 
     const hash = bcrypt.hashSync(password);
 
+    console.log(email, name, hash);
+
     db.transaction(trx => {
       trx.insert({
         hash: hash, 
