e821aa44749f635ee3dee8d3d3306a1f447b5e9a
ggroce <ggrocedev@gmail.com>
2021-01-11 18:58:10 -0600

    debug

--- server.js
+++ server.js
@@ -10,10 +10,12 @@ const image = require('./controllers/image');
 const db = require('knex')({
   client: 'pg',
   connection: {
-    connectionString : process.env.DATABASE_URL,
+    connectionString: process.env.DATABASE_URL,
     ssl: true
   }
 });
+
+console.log(db);
 
 const app = express();
 app.use(express.json());
